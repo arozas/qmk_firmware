54e6242f599ee5642c43dfd6978be3b2deec048a
Alejandro Alberto Martín Rozas <alejandro.rozas@gmail.com>
2023-01-22 19:50:25 -0300

    Avanzando con la configuración del teclado.

--- keyboards/crkbd/keymaps/arozas_corne/config.h
+++ keyboards/crkbd/keymaps/arozas_corne/config.h
@@ -50,3 +50,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+#define FORCE_NKRO
--- keyboards/crkbd/keymaps/arozas_corne/rules.mk
+++ keyboards/crkbd/keymaps/arozas_corne/rules.mk
@@ -1,8 +1,17 @@
-MOUSEKEY_ENABLE 	= yes    # Mouse keys
+
 RGB_MATRIX_ENABLE 	= yes # Enable WS2812 RGB underlight.
+
 OLED_ENABLE     	= yes
 OLED_DRIVER     	= SSD1306
-LTO_ENABLE      	= yes
 
+UNICODE_ENABLE		= yes
+UNICODEMAP_ENABLE 	= yes
+UCIS_ENABLE			= yes
+
+KEY_LOCK_ENABLE		= yes
+
+NKRO_ENABLE			= yes
 TAP_DANCE_ENABLE	= yes
 WPM_ENABLED			= yes
+LTO_ENABLE      	= yes
+MOUSEKEY_ENABLE 	= no    # Mouse keys
